bc948b946d6924873423890a6272ae55eb6f081d
Pavan K Rao <raopavank@gmail.com>
2022-09-17 19:06:45 +0530

    InspectorWindowFixed
    (cherry picked from commit b10d1cb39e9d3a5afb704a6f2e4342786d01dc1c)

--- AI  Project/Assets/Scripts/GOAP/Editor/GOAP_VisualizerEditor.cs
+++ AI  Project/Assets/Scripts/GOAP/Editor/GOAP_VisualizerEditor.cs
@@ -6,6 +6,8 @@ using UnityEditor.UIElements;
 
 public class GOAP_VisualizerEditor : EditorWindow
 {
+    private GraphViewUI graph;
+    private VisualElement element;
     [MenuItem("GOAP/Debug/Visualize")]
     public static void ShowExample()
     {
@@ -17,14 +19,40 @@ public class GOAP_VisualizerEditor : EditorWindow
     {
         // Each editor window contains a root VisualElement object
         VisualElement root = rootVisualElement;
-
+        
         var splitView = new TwoPaneSplitView(0, 250, TwoPaneSplitViewOrientation.Horizontal);
-        root.Add(splitView); 
-        var leftPane = new ListView();
-        splitView.Add(leftPane);
+        root.Add(splitView);
+
+        var inspector = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>("Assets/Scripts/GOAP/Editor/Goap_Inspector.uxml");
+        VisualElement inspectorUI = inspector.CloneTree();
+        splitView.Add(inspectorUI);
+        element = inspectorUI;
+
         var visualTree = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>("Assets/Scripts/GOAP/Editor/GOAP_VisualizerEditor.uxml");
-        VisualElement labelFromUXML = visualTree.Instantiate();
-        splitView.Add(labelFromUXML);
+        VisualElement graphViewUI = visualTree.CloneTree();
+        graph = graphViewUI.Q<GraphViewUI>("Graph");
+        splitView.Add(graphViewUI);
+
+
+
+        var node = new NodeViewUI()
+        {
+            name = "node",
+            title = "hello!"
+        };
+
+        //node.titleButtonContainer.Add(new Button() { text = "Click Me!" });
+        //node.titleContainer.Add(new Button() { text = "Click Me!" });
+        //node.extensionContainer.Add(new Label("Hello extension!"));
+        //node.inputContainer.Add(new Label("Hello input!"));
+        //node.mainContainer.Add(new Label("Hello main!"));
+        //node.outputContainer.Add(new Label("Hello output!"));
+        //node.topContainer.Add(new Label("Hello Top!"));
+        //node.contentContainer.Add(new Label("Hello Content!"));
+        //graph.AddElement(node);
+        //splitView.Add(graphViewUI);
+        //node.RefreshPorts();
+        //node.RefreshExpandedState();
 
         var styleSheet = AssetDatabase.LoadAssetAtPath<StyleSheet>("Assets/Scripts/GOAP/Editor/GOAP_VisualizerEditor.uss");
         root.styleSheets.Add(styleSheet);
