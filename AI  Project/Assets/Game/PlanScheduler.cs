4d1fa0a15d981437a0da1bb5248379bbb68ea0f1
Pavan K Rao <raopavank@gmail.com>
2022-10-11 19:39:20 +0530

    undo delete

---
+++ AI  Project/Assets/Game/PlanScheduler.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanScheduler
+{
+    public void EnactPlan(NPC_Agent agent, Plan plan)
+    {
+
+    }
+}
